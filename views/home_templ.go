b21d6c37782d51c1fefd1e706e82180fec7bba0e
lzadhito <lzadhito@gmail.com>
2024-06-10 22:42:38 +0700

    feat: init alpine js

--- views/home_templ.go
+++ views/home_templ.go
@@ -118,7 +118,7 @@ func Card(habit string) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(habit)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/home.templ`, Line: 44, Col: 22}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/home.templ`, Line: 46, Col: 22}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
